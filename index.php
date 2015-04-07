4a6f698aeed1f433e2941cb7ca9c9cfd82217c76
tomson2001 <tomson2001@yahoo.de>
2015-04-07 22:09:21 +0200

    Webtemplate added. Based on Bootstrap 3.3.4

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php 
-include 'mapping.php'; 
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<meta charset="utf-8">
+  	<title>RefModMining</title>
+   	<link rel="stylesheet" href="gui/lib/bootstrap-3.3.4-dist/css/bootstrap.min.css">
+   	<link rel="stylesheet" href="gui/lib/bootstrap-3.3.4-dist/css/bootstrap-theme.min.css">
+   	<link href="gui/css/navbar-fixed-top.css" rel="stylesheet">
+   	<script type="text/javascript" src="gui/lib/jQuery-2.1.3/jquery-2.1.3.min.js"></script>
+   	<script src="gui/lib/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php include 'gui/main.php'; ?>
+</body>
+</html>
